99aa3b847cd73c1b1dd9634409145ef47c36521d
rob96753 <rob99202@gmail.com>
2023-05-30 15:00:15 -1000

    Working version of the book review

---
+++ Resources/Thumbnails.js
@@ -0,0 +1,32 @@
+var thumbnails = [
+        {
+            "title": "Harry Potter and the Philosopher’s Stone",
+            "thumbnail": "Resources/HarryPotter2.jpg",
+            "key": "harrypotter"
+        },
+        {
+            "title": "Ender’s Game ",
+            "thumbnail": "Resources/EndersGame2.jpg",
+            "key": "endersgame"
+        },
+        {
+            "title": "A Good Girl's Guide To Murder",
+            "thumbnail": "Resources/AGoodGirlsGuideToMurder.jpg",
+            "key": "agoodgirlsguide"
+        },
+        {
+            "title": "The Giver",
+            "thumbnail": "Resources/TheGiver.jpg",
+            "key": "thegiver"
+        },
+        {
+            "title": "Project Hail Mary",
+            "thumbnail": "Resources/ProjectHailMary.jpg",
+            "key": "projecthailmary"
+        },
+        {
+            "title": "The Martian",
+            "thumbnail": "Resources/TheMartian.jpg",
+            "key": "themartian"
+        }
+        ];
